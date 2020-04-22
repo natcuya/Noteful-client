097516338cf961929fb8e1ec6fb8d83030276e9d
Nathaly Cuya <nathalycuya@utexas.edu>
2020-04-22 00:54:03 -0500

    delete

--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -24,7 +24,7 @@ export default class NoteListNav extends React.Component {
                 to={`/folder/${folder.id}`}
               >
                 <span className='NoteListNav__num-notes'>
-              {notes.filter(note => note.folder === Number(folder.id)).length}
+                {countNotesForFolder(notes, folder.id)}
                 </span>
                 {folder.title}
               </NavLink>
